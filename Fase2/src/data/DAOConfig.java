0e836d111a551e5d6cb71e0b1b2927933e3d1d89
João Pedro Baptista <102752884+joaobaptista03@users.noreply.github.com>
2023-12-29 18:33:13 +0000

    DAOConfig.java

---
+++ Fase2/src/data/DAOConfig.java
@@ -0,0 +1,9 @@
+package src.data;
+
+public class DAOConfig {
+    static final String USERNAME = "root";
+    static final String PASSWORD = "Jopemoba@3642mysql";
+    //private static final String DRIVER = "jdbc:mariadb";
+    private static final String DRIVER = "jdbc:mysql";
+    static final String URL = DRIVER+"://localhost:3306/";
+}
